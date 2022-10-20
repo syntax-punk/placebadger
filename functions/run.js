5be0a08f30f374c5b6ef2b17935278c760e2386f
theVoogie <vugar.abdulzada@webstep.no>
2022-10-20 17:11:00 +0200

    Added imgs and stuff

--- functions/run.js
+++ functions/run.js
@@ -1,10 +1,53 @@
+const playwright = require("playwright-aws-lambda");
+
+const getUrl = () => {
+  const badgerPath = "https://gorgeous-genie-fdd533.netlify.app/badger";
+
+  // const {
+  //   cardpath = "https://gorgeous-genie-fdd533.netlify.app",
+  //   title = "No Title",
+  //   handle = "",
+  //   tags = "", 
+  //   width = 1200,
+  //   height = 630,
+  // } = queryStringParameters;
+  // return `${cardpath}?title=${title}&handle=${handle}&tags=${tags}&width=${width}&height=${height}`
+  return badgerPath;
+};
+
 async function handlerFunction(event, ctx) {
 
+  const badgerPath = getUrl(); 
+
+  const browser = await playwright.launchChromium();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(url);
+
+  const bbox = await page.evaluate(() => {
+    const root = document.getElementById("badger");
+    const { x, y, width, height } = root.children[0].getBoundingClientRect();
+    return { x, y, width, height };
+  });
+
+  const badgerBuffer = await page.screenshot({ clip: bbox });
+  await browser.close();
+
+  // return {
+  //   statusCode: 302,
+  //   headers: {
+  //       "Location": "https://gorgeous-genie-fdd533.netlify.app/badger"
+  //   }
+  // }
+
   return {
-    statusCode: 302,
+    isBase64Encoded: true,
+    statusCode: 200,
     headers: {
-        "Location": "https://gorgeous-genie-fdd533.netlify.app/badger"
-    }
+      "Content-Type": "image/jpg",
+      "Content-Length": badgerBuffer.length.toString()
+    },
+    body: badgerBuffer.toString("base64")
   }
 }
 
